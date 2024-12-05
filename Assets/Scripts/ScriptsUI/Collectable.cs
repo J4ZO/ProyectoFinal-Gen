9e348de1131629b12241e0a4b54883293c96ee3c
JaiderZ <jjaider.zos@gmail.com>
2024-12-05 16:39:23 -0500

    Player Animations

--- Assets/Scripts/ScriptsUI/Collectable.cs
+++ Assets/Scripts/ScriptsUI/Collectable.cs
@@ -1,20 +1,64 @@
+using System.Collections;
 using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
     public InventoryItem item; // Asocia un objeto inventariable
+    private Animator animator;
+    
+    public bool isPicked;
+    private bool isPlayerInRange;
+
+    private void Update()
+    {
+        // Verifica si el jugador está cerca y presiona E
+        if (isPlayerInRange && Input.GetKeyDown(KeyCode.E) && !isPicked)
+        {
+            isPicked = true;
+            Debug.Log("Recoge objeto");
+            StartCoroutine(PickItem());
+        }
+    }
+
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            Inventory playerInventory = other.GetComponent<Inventory>();
-            if (playerInventory != null)
-            {
-                playerInventory.AddItem(item);
-                Destroy(gameObject); // Elimina el objeto del juego
-            }
+            animator = other.GetComponent<Animator>();
+            isPlayerInRange = true;
+            Debug.Log("player in range"); 
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("El jugador salió de rango");
+            isPlayerInRange = false;
+        }
+    }
+
+    private IEnumerator PickItem()
+    {
+        if (animator != null)
+        {
+            animator.SetTrigger("Pick");
+        }
+
+        yield return new WaitForSeconds(3f); 
+
+        
+        Inventory playerInventory = FindObjectOfType<Inventory>();
+        if (playerInventory != null)
+        {
+            playerInventory.AddItem(item);
+        }
+
+        Destroy(gameObject);
+        isPicked = false; 
     }
 }
 
