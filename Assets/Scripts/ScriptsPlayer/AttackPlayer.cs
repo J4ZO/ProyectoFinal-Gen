8e4abd8b6c2d16cb2ee28d3b5dc49f5f80a30319
Adriana-94 <adrianakalderon94@gmail.com>
2024-12-06 12:07:41 -0500

    Agregando ataques

--- Assets/Scripts/ScriptsPlayer/AttackPlayer.cs
+++ Assets/Scripts/ScriptsPlayer/AttackPlayer.cs
@@ -9,7 +9,9 @@ public class AttackPlayer : MonoBehaviour
     [SerializeField] GameObject gun;
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private float bulletPower = 20f;
+    [SerializeField] private Collider punchCollider;
     public float damageAmount = 5f;
+    private bool gunActive = true;
 
 
     // Start is called before the first frame update
@@ -33,7 +35,7 @@ public class AttackPlayer : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            if (gunActive) 
             {
                 AtacarConPistola();
             }
@@ -41,14 +43,21 @@ public class AttackPlayer : MonoBehaviour
             {
                 AtacarConPunos();
             }
+            if (Input.GetKeyDown(KeyCode.R))
+            { 
+                ToggleGun();
+            }
         }
     }
 
     private void AtacarConPunos()
     {
-        // Se debe poner un colisionador a los puños para que al entrar en contacto con el enemigo se ejecute el daño
+      
         animator.SetTrigger("Punch"); 
         Debug.Log("Atacando con puños");
+
+        punchCollider.enabled = true;
+        StartCoroutine(DesactivarColisionador(punchCollider, 0.5f));
 
         if (healthEnemy != null) 
         { 
@@ -59,21 +68,21 @@ public class AttackPlayer : MonoBehaviour
 
     private void AtacarConPistola()
     {
-        // Se debe poner un colisionador a las balas para que al entrar en contacto con el enemigo se ejecute el daño
+        punchCollider.enabled = false;
         GameObject bullet = Instantiate(bulletPrefab, gun.transform.position, gun.transform.rotation); 
         Rigidbody rb = bullet.GetComponent<Rigidbody>(); 
         rb.AddForce(gun.transform.forward * bulletPower, ForceMode.Impulse);
         Debug.Log("Atacando con pistola"); 
     }
-
-    //Organizar que desactive y active, ahi puse para ocultar la pistola, como ejemplo
-    private void HideGun()
-    {
-        gun.SetActive(false);
+    private void ToggleGun() 
+    { 
+        gunActive = !gunActive; 
+        gun.SetActive(gunActive); 
+        Debug.Log(gunActive ? "Sacando el arma" : "Guardando el arma"); 
     }
-
     private IEnumerator DesactivarColisionador(Collider collider, float delay) 
     {
-        yield return new WaitForSeconds(delay); collider.enabled = false; 
+        yield return new WaitForSeconds(delay); 
+        collider.enabled = false; 
     }
 }
