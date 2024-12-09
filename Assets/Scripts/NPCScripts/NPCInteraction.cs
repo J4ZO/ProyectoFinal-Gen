e6eed7bc0cf469d91d3b64497d316c8ca0b7cec2
marvisa22 <107163520+marvisa22@users.noreply.github.com>
2024-12-09 10:32:11 -0500

    infoInteractivo
    Se crea este  nuevo texto dentro del canvas level1_final

--- Assets/Scripts/NPCScripts/NPCInteraction.cs
+++ Assets/Scripts/NPCScripts/NPCInteraction.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 
 public class NPCInteraction : MonoBehaviour
 {
-    [SerializeField] private GameObject textNPC;
-    [SerializeField] private List<GameObject> texts;
+    [SerializeField] private GameObject textNPC; // Texto del diálogo del NPC
+    [SerializeField] private List<GameObject> texts; // Lista de mensajes de diálogo
+    [SerializeField] private GameObject interactionCanvas; // Canvas que muestra la tecla de interacción
+
     private int indexTexts = 0;
     public bool isPlayerInRange;
 
@@ -20,33 +22,50 @@ public class NPCInteraction : MonoBehaviour
     {
         animator = GetComponent<Animator>();
         dialog = GetComponent<DialogFragmentController>();
+
+        if (interactionCanvas != null)
+        {
+            interactionCanvas.SetActive(false); // Asegúrate de que el canvas esté apagado al inicio
+        }
     }
-    // Update is called once per frame
+
     void Update()
     {
-        if (isPlayerInRange && Input.GetKeyDown(KeyCode.F))
+        if (isPlayerInRange)
         {
-            animator.SetTrigger("isTalking");
-            textNPC.SetActive(true);
+            if (interactionCanvas != null && !interactionCanvas.activeSelf)
+            {
+                interactionCanvas.SetActive(true); // Muestra el canvas cuando el jugador está cerca
+            }
 
-            // Avanza el diálogo al siguiente fragmento
-            dialog.ShowNextFragment();
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                animator.SetTrigger("isTalking");
+                textNPC.SetActive(true);
+
+                // Avanza el diálogo al siguiente fragmento
+                dialog.ShowNextFragment();
+            }
         }
+        else if (interactionCanvas != null && interactionCanvas.activeSelf)
+        {
+            interactionCanvas.SetActive(false); // Oculta el canvas cuando el jugador se aleja
+        }
+
         ShowMessages();
     }
 
-
-    private void OnTriggerEnter(Collider other) 
+    private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Player") )
+        if (other.gameObject.CompareTag("Player"))
         {
             isPlayerInRange = true;
         }
     }
 
-    private void OnTriggerExit(Collider other) 
+    private void OnTriggerExit(Collider other)
     {
-        if(other.gameObject.CompareTag("Player") )
+        if (other.gameObject.CompareTag("Player"))
         {
             isPlayerInRange = false;
             textNPC.SetActive(false);
@@ -58,11 +77,11 @@ public class NPCInteraction : MonoBehaviour
 
     private void ShowMessages()
     {
-        if(dialog.IsDialogComplete && isPlayerInRange && textNPC.activeSelf)
+        if (dialog.IsDialogComplete && isPlayerInRange && textNPC.activeSelf)
         {
             texts[indexTexts].SetActive(false);
             indexTexts++;
-            if(indexTexts < texts.Count)
+            if (indexTexts < texts.Count)
             {
                 texts[indexTexts].SetActive(true);
             }
@@ -74,6 +93,6 @@ public class NPCInteraction : MonoBehaviour
                 isInteractionCompleated = true;
             }
         }
-
     }
 }
+
